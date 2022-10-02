e5fbca11ee67e933805bd9702320fb6df01bb50f
조시헌 <56191064+joesiheon496@users.noreply.github.com>
2022-10-02 13:52:39 +0900

    Update ConcatFile.py

--- Python/ConcatFile.py
+++ Python/ConcatFile.py
@@ -4,5 +4,5 @@ forders = os.listdir('파일위치')
 df_all_data = pd.DataFrame()
 
 for files in forders[:-1]:
-	df = pd.read_csv(f'위치',dtype = {'열의 데이터 형태 지정'}) #,parse_dates=['Date']
+	df = pd.read_csv(f'파일위치/{files}',dtype = {'열의 데이터 형태 지정'}) #,parse_dates=['Date']
 	df_all_data = pd.concat([df_all_data,df])
